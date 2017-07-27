eefed94b03ee3b68e7cf810ed1ea505ab7c70bd1
devarpan <30466176+devarpan@users.noreply.github.com>
2017-07-27 09:57:33 +0530

    next commit

--- myplugin/test.php
+++ myplugin/test.php
@@ -37,7 +37,7 @@ function my_first_menu_page(){
 		$table_name='counter';
 		$insertResult=$wpdb->insert($table_name,array('counter_name'=> $counterName,'counter_base_val'=> $counterBaseVal,'counter_incrment_with'=> $counter_incrVal,'number_of_days'=> $counter_incrDay,'start_date'=> $counter_incrDate));
 		$lastid = $wpdb->insert_id;
-				
+		
 			$query = "SELECT * FROM $table_name where id = ".$lastid;
 			$result = $wpdb->get_results($query);
 			
